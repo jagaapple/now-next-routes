409287593d7483e1d8fd76e10b35faf7ff3c86f6
Jaga Apple <jagaapple@uniboar.com>
2019-06-01 20:20:16 +0900

    [Add] Add some pages and configure routes for example project

---
+++ example/pages/comments/comment.tsx
@@ -0,0 +1,46 @@
+import { NextFC } from "next";
+import Router from "next/router";
+import Link from "next/link";
+
+import routes from "../../routes";
+
+type InitialProps = {
+  userId: number;
+  commentId: number;
+};
+
+const Component: NextFC<InitialProps> = (props: InitialProps) => {
+  const userLinkProps = routes["users/user"].getLinkProps({ userId: props.userId });
+
+  return (
+    <main>
+      <h1>now-next-routes-example</h1>
+      <h2>
+        /users/user/{props.userId}/comments/{props.commentId}
+      </h2>
+
+      <Link {...userLinkProps}>
+        <a>Back to user ({props.userId})</a>
+      </Link>
+    </main>
+  );
+};
+
+type Query = ReturnType<typeof routes["comments/comment"]["getLinkProps"]>["href"]["query"];
+Component.getInitialProps = ({ query }: { query: Query }) => {
+  const userId = Number(query.userId);
+  const commentId = Number(query.commentId);
+
+  if (Number.isNaN(userId)) {
+    const route = routes["users/users"].getLinkProps({});
+    Router.push(route.href, route.as);
+  }
+  if (Number.isNaN(commentId)) {
+    const route = routes["users/user"].getLinkProps({ userId });
+    Router.push(route.href, route.as);
+  }
+
+  return { userId, commentId };
+};
+
+export default Component;
--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,3 +1,22 @@
-const Component = () => <h1>Hello</h1>;
+import Link from "next/link";
+
+import routes from "../routes";
+
+const usersLinkProps = routes["users/users"].getLinkProps({});
+
+const Component = () => (
+  <main>
+    <h1>now-next-routes-example</h1>
+    <h2>/index</h2>
+
+    <ul>
+      <li>
+        <Link {...usersLinkProps}>
+          <a>users</a>
+        </Link>
+      </li>
+    </ul>
+  </main>
+);
 
 export default Component;
