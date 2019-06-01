c54dc4e4c02959332a89741d236eef45b7b291f7
Jaga Apple <jagaapple@uniboar.com>
2019-06-01 21:23:19 +0900

    [Add] Add documentation comments

--- src/route.ts
+++ src/route.ts
@@ -1,16 +1,27 @@
 import { createStringWithLeadingSlash } from "./utilities";
 
 type Settings = {
+  /** A page file path in `/pages` directory of Next.js. */
   page: string;
+  /**
+   * A mapping path pattern.
+   * The format `:parameterName` will be mapped to dynamic parameters.
+   */
   pattern: string;
-  as?: string;
 };
 
 type LinkProps<Parameters> = {
+  /** A mapped path information. */
   href: {
+    /**
+     * A base path name.
+     * This is a page file path in `/pages` in general.
+     */
     pathname: string;
+    /** Displayed URL string in web browsers. */
     query: Partial<Parameters>;
   };
+  /** A mapped URL. */
   as: string;
 };
 
@@ -28,6 +39,7 @@ export class Route<Parameters extends object = Record<string, number | string>> 
   // ---------------------------------------------------------------------------------------------------------------------------
   // Constructors
   // ---------------------------------------------------------------------------------------------------------------------------
+  /** Creates a new route which can create props for `<Link>` component of Next.js. */
   constructor(settings: Settings) {
     if (typeof settings !== "object") throw new TypeError("Invalid argument type");
 
@@ -46,6 +58,7 @@ export class Route<Parameters extends object = Record<string, number | string>> 
 
   // Public Functions
   // ---------------------------------------------------------------------------------------------------------------------------
+  /** Gets props for `<Link>` component of Next.js. */
   getLinkProps(parameters: Parameters): LinkProps<Parameters> {
     const query = Object.keys(parameters).reduce((object: Partial<Parameters>, key: string) => {
       const parameterName = key as keyof Parameters;
@@ -68,6 +81,10 @@ export class Route<Parameters extends object = Record<string, number | string>> 
     };
   }
 
+  /**
+   * Creates an object for `routes` propety in `now.json` for ZEIT Now.
+   * This method is used in CLI, so **you don't need to use this method in general**.
+   */
   createRouteForNow() {
     const regExpRoutePattern = this.settings.pattern.replace(/\//g, "\\/").replace(/:[^\\/]+/g, "([^\\/]+?)");
     const src = `^${regExpRoutePattern}\\/?$`.replace(/(\\\/)+/g, "\\/").replace(/^\^\\\\\/?\$$/, "\\/");
